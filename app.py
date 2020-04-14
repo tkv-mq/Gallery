50b61eae7bd233c80527b3ddfdcea0fec969ebc1
Mq <toktorovm@gmail.com>
2020-04-14 20:26:03 +0600

    exam #1-st vesion

---
+++ app.py
@@ -0,0 +1,24 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    note = open("note.txt", "r+", encoding="utf-8")
+    photos = [i for i in note]
+    note.close()
+    return render_template("home.html", photos=photos)
+
+@app.route("/add")
+def add():
+    return render_template("add.html")
+
+@app.route("/add-ok", methods=["POST"])
+def add_photo():
+    note = request.form
+    note_file = open("note.txt", "a+", encoding="utf-8")
+    data = note["url"]
+    note_file.write(data)
+    note_file.close()
+    return render_template("ok.html")
+

6823fc3042abe4c6cab7dbbef3b5051673b98658
Mq <toktorovm@gmail.com>
2020-04-14 21:13:24 +0600

    4

--- app.py
+++ app.py
@@ -3,6 +3,9 @@ from flask import Flask, render_template, request
 app = Flask(__name__)
 
 @app.route("/")
+def home():
+    return render_template("home.html")
+@app.route("/gallery")
 def gallery():
     note = open("note.txt", "r+", encoding="utf-8")
     photos = [i for i in note]

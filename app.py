9e13048f6d45c41fc254ec1ed071d66b4c0f02ff
Mq <toktorovm@gmail.com>
2020-04-14 20:52:12 +0600

    debug

--- app.py
+++ app.py
@@ -2,12 +2,12 @@ from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-@app.route("/")
-def home():
+@app.route("/gallery")
+def gallery():
     note = open("note.txt", "r+", encoding="utf-8")
     photos = [i for i in note]
     note.close()
-    return render_template("home.html", photos=photos)
+    return render_template("gallery.html", photos=photos)
 
 @app.route("/add")
 def add():
@@ -17,7 +17,7 @@ def add():
 def add_photo():
     note = request.form
     note_file = open("note.txt", "a+", encoding="utf-8")
-    data = note["url"]
+    data = note["url"] + '\n'
     note_file.write(data)
     note_file.close()
     return render_template("ok.html")
---
+++ euler16.py
@@ -0,0 +1,8 @@
+num = 2**15
+res = 0
+nums = list(str(num))
+for i in nums:
+    i = int(i)
+    res += i
+
+print(res)
